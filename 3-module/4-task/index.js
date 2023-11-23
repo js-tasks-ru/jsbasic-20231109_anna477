973fe9db4428f34f967401aec81920d42081beb0
Anna <shcerbova17@gmail.com>
2023-11-23 14:51:18 +0100

    Completed task 4 in module 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,14 @@
 function showSalary(users, age) {
   // ваш код...
+  let resultString = '';
+  const filterUsers = users.filter(user => user.age <= age);
+  filterUsers.forEach((user, i) => {
+    resultString += `${user.name}, ${user.balance}`;
+    if (i < (filterUsers.length - 1)) {
+      resultString += `\n`;
+    }
+  });
+
+  return resultString;
 }
+
