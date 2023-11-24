ef1627f2ee653413cee3b96f2c0a2a4653b2ee9c
Anna <shcerbova17@gmail.com>
2023-11-24 22:37:06 +0100

    Completed task 3 in module 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
   // ваш код...
+  const trs = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+  Array.from(trs).forEach((tr) => {
+    const status = tr.getElementsByTagName('td')[3];
+    if (status.getAttribute('data-available') === 'true') {
+      tr.classList.add('available');
+    } else if (status.getAttribute('data-available') === 'false') {
+      tr.classList.add('unavailable');
+    } else {
+      tr.setAttribute('hidden', '');
+    }
+
+    const gender = tr.getElementsByTagName('td')[2];
+    if (gender.innerText === 'm') {
+      tr.classList.add('male');
+    }
+    if (gender.innerText === 'f') {
+      tr.classList.add('female');
+    }
+
+    const age = tr.getElementsByTagName('td')[1];
+    if (+age.innerText < 18) {
+      tr.style.textDecoration = 'line-through';
+    }
+  });
+
 }
