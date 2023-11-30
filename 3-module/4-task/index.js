444d79ff088c6ccec8e3bdbb5827016c87c65a51
Anna <shcerbova17@gmail.com>
2023-11-30 11:19:37 +0100

    Completed task 4 in module 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,14 +1,19 @@
 function showSalary(users, age) {
   // ваш код...
-  let resultString = '';
-  const filterUsers = users.filter(user => user.age <= age);
-  filterUsers.forEach((user, i) => {
-    resultString += `${user.name}, ${user.balance}`;
-    if (i < (filterUsers.length - 1)) {
-      resultString += `\n`;
-    }
-  });
-
-  return resultString;
+  return users
+    .filter((user) => user.age <= age)
+    .map((user, i) => {
+      return `${user.name}, ${user.balance}`;
+    })
+    .join('\n');
 }
+
+//второе решение
+// filterUsers.forEach((user, i) => {
+//   resultString += `${user.name}, ${user.balance}`;
+//   if (i < (filterUsers.length - 1)) {
+//     resultString += `\n`;
+//   }
+// });
+// return resultString;
 
