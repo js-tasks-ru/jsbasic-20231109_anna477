834369df19fe0950e5e5a6f293ba369ed33e7f2b
Anna <shcerbova17@gmail.com>
2023-12-01 14:46:04 +0100

    Completed task 2 in module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
   // ваш код...
+  const clickBtn = document.querySelector(".toggle-text-button");
+  const text = document.querySelector('#text');
+
+  clickBtn.addEventListener("click", action);
+
+  function action() {
+    text.hidden = !text.hidden;
+  }
 }
