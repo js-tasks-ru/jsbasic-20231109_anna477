318fb0eaed1d7b1247db2f0cf0524230d9ef2bcd
Anna <shcerbova17@gmail.com>
2023-12-03 16:29:40 +0100

    Completed task 3 in module 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  inner = document.querySelector('.carousel__inner');
+  rightBtn = document.querySelector('.carousel__arrow_right');
+  leftBtn = document.querySelector('.carousel__arrow_left');
+  stepCounter = 0;
+  imgStep = 500;
+  leftBtn.style.display = 'none';
+  slides = document.querySelectorAll('.carousel__slide');
+
+  slides.forEach(slide => {
+    slide.style.width = '500px';
+    slide.querySelector('img').style.width = '500px';
+  });
+
+  rightBtn.addEventListener('click', clickRight);
+
+  leftBtn.addEventListener('click', clickLeft);
+}
+
+function clickRight() {
+  console.log('clickRight');
+  stepCounter += imgStep;
+  leftBtn.style.display = '';
+  if (stepCounter >= 1500) {
+    rightBtn.style.display = 'none';
+  }
+  inner.style.transform = `translateX(-${stepCounter}px)`;
+}
+
+function clickLeft() {
+  console.log('clickLeft');
+  stepCounter -= imgStep;
+  rightBtn.style.display = '';
+  if (stepCounter <= 0) {
+    leftBtn.style.display = 'none';
+  }
+  inner.style.transform = `translateX(-${stepCounter}px)`;
 }
